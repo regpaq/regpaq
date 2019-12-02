890a5b786e373bf62b727dce3126dc644ecfc29b
regpaq <reggie@regpaq.com>
2019-12-01 22:18:49 -0800

    Using Gulp to compress images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 const gulp = require('gulp');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const jp2 = require('gulp-jpeg-2000');
 
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+
+// image processing
+function images() {
+
+    const out = 'assets/images/';
+
+    return gulp.src('_rawAssets/*')
+        .pipe(newer(out))
+        .pipe(imagemin([
+            mozjpeg({quality: 80}),
+            imagemin.optipng({optimizationLevel: 5}),    
+        ],
+        { verbose: true }
+        ))
+        .pipe(gulp.dest(out));
 }
 
-exports.default = defaultTask
+exports.images = images;
